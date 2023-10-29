f526f8a610d94fb165f67283b180dda6dfa55fb6
FahmiRid <95422790+FahmiRid@users.noreply.github.com>
2023-10-30 04:36:17 +0800

    adding icon map

--- src/Components/BottomNavigation.jsx
+++ src/Components/BottomNavigation.jsx
@@ -2,20 +2,29 @@
 import React from 'react';
 import '../styles/BottomNavigation.css';
 import Lottie from 'lottie-react'
+import Map from '../Assets/map.png';
 
 function BottomNavigation() {
-  return (
-    <div className="bottom-navigation">
-      <div className="nav-item">
-      Maps
-      </div>
-      <div className="nav-item">
-        Reminder
-      </div>
-      <div className="nav-item">Contact</div>
-      <div className="nav-item">Greetings</div>
-    </div>
-  );
+    return (
+        <div className="bottom-navigation">
+            <div className="nav-item">
+                <img className='img-class' src={Map} alt='map' width={20} height={20} />
+                Maps
+            </div>
+            <div className="nav-item">
+                <img  className='img-class' src={Map} alt='map' width={20} height={20} />
+                Reminder
+            </div>
+            <div className="nav-item">
+                <img className='img-class' src={Map} alt='map' width={20} height={20} />
+                Contact
+            </div>
+            <div className="nav-item">
+                <img className='img-class' src={Map} alt='map' width={20} height={20} />
+                Greetings
+            </div>
+        </div>
+    );
 }
 
 export default BottomNavigation;
